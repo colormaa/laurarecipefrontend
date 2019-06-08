6c1b086b6dd2fbdb053d33c7ad9fe6f2a1ddc382
colormaa <colormaa@gmail.com>
2019-06-08 03:59:45 -0700

    a

--- src/store.js
+++ src/store.js
@@ -12,7 +12,8 @@ const persistedState = loadState();
 const store =  createStore(rootReducer, 
     persistedState, 
     compose(applyMiddleware(...middleware), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
     );
 store.subscribe(
     throttle(
